b825e0bcb1e96f4eba7b4e3b2a8c6dfc03d11fac
Eduardo Nascimento <eduardosgn19@gmail.com>
2023-10-20 10:29:24 -0300

    boardgame Position class added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,2 +1,11 @@
-package application;public class Main {
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(25, 60);
+
+        System.out.println(pos);
+    }
 }
